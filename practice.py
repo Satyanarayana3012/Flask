49e7c984207a539c293b9b6752e7ca5844c20b6e
Satyanarayana3012 <satya30yp@gmail.com>
2023-11-16 10:53:48 +0530

    ''

--- practice.py
+++ practice.py
@@ -15,18 +15,18 @@
 
 #Question 2
 
-class Shape:
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+# class Shape:
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
 
-    def area(self):
-        return 0
+#     def area(self):
+#         return 0
 
-class Rectangle(Shape):
-    def area(self):
-        return self.width * self.height
+# class Rectangle(Shape):
+#     def area(self):
+#         return self.width * self.height
 
-rectangle = Rectangle(5, 8)
+# rectangle = Rectangle(5, 8)
 
-print(rectangle.area()) 
+# print(rectangle.area()) 
