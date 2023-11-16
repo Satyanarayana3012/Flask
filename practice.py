15cc65bf8af7a2e8afa79e26bc4dd5a61eb14563
Satyanarayana3012 <satya30yp@gmail.com>
2023-11-16 10:48:09 +0530

    Second Question

--- practice.py
+++ practice.py
@@ -1,13 +1,29 @@
-class Animal:
-    def speak(self):
-        print("Animal speaks")
+# class Animal:
+#     def speak(self):
+#         print("Animal speaks")
 
-class Dog(Animal):
-    def speak(self):
-        print("Dog barks")
+# class Dog(Animal):
+#     def speak(self):
+#         print("Dog barks")
 
-animal = Animal()
-animal.speak()
+# animal = Animal()
+# animal.speak()
 
-dog = Dog()
-dog.speak()
+# dog = Dog()
+# dog.speak()
+
+class Shape:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return 0
+
+class Rectangle(Shape):
+    def area(self):
+        return self.width * self.height
+
+rectangle = Rectangle(5, 8)
+
+print(rectangle.area()) 
